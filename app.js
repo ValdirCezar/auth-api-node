3fe1a7859bdacaafb7aa9f5d982aa0b03385de82
Valdir Cezar <valdircezar312@gmail.com>
2021-11-05 15:40:07 -0300

    add UserRoutes

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 import express from "express"
-
 import * as db from "./src/config/db/initialData.js"
+import userRoutes from "./src/modules/user/routes/UserRoutes.js";
 
 const app = express();
 const env = process.env;
 const PORT = env.PORT || 8080
 
 db.createInitialData();
+
+app.use(userRoutes);
+app.use(express.json());
 
 app.get('/api/status', (req, res) => {
     return res.json({
